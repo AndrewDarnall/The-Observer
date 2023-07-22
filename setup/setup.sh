6dee8a88efdc0961d75daf5a1bccbabc66f69fcd
TheComputerScientist <DRNNRW00M10C351S@studium.unict.it>
2023-07-22 13:13:11 +0200

    fixed the setup file

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # setup script to prepare the environment, for ease of execution
-docker volume create datastorage && docker network create tap_project
+docker volume create datastorage && docker network create --subnet=10.0.100.1/24 tap_project_2
