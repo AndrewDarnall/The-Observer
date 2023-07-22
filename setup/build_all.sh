818ca62c97dfbfd18521d8c0f25a6f119cf08334
TheComputerScientist <DRNNRW00M10C351S@studium.unict.it>
2023-07-22 13:19:19 +0200

    polished config files

--- setup/build_all.sh
+++ setup/build_all.sh
@@ -2,4 +2,4 @@
 
 # Simple build script for ease of deployment
 
-docker build ./stream_connector/Dockerfile -t tap_project:stream_connector_2 && docker build ./data_ingestor/Dockerfile -t tap_project:data_ingestor && docker build ./data_streamer --tag tap_project:kafka
+docker build ./stream_connector/ -t tap_project:stream_connector_2 && docker build ./data_ingestor/ -t tap_project:data_ingestor && docker build ./data_streamer --tag tap_project:kafka
