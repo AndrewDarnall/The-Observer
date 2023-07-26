5f75aa8b23c6e986d6be044ac68897d52bf5db65
TheComputerScientist <DRNNRW00M10C351S@studium.unict.it>
2023-07-26 23:37:24 +0200

    dir polish 1

--- get_kafka.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -P ./Data_Streaming/setup https://dlcdn.apache.org/kafka/3.4.1/kafka_2.13-3.4.1.tgz
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# setup script to prepare the environment, for ease of execution
-docker volume create datastorage && docker network create --subnet=10.0.100.1/24 tap_project_2
