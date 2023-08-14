6891f3931621d4753b42f4de94ba6e665761ae21
TheComputerScientist <DRNNRW00M10C351S@studium.unict.it>
2023-08-14 02:15:19 +0200

    fixed setup file

--- project_setup.sh
+++ project_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -P ./Data_Streaming/setup/ https://dlcdn.apache.org/kafka/3.4.1/kafka_2.13-3.4.1.tgz
-wget -P ./Data_Processing/setup/ https://dlcdn.apache.org/spark/spark-3.4.0/spark-3.4.0-bin-hadoop3.tgz
-docker network create --subnet=10.0.100.1/24 tap_project_2
-docker volume create datastorage
+wget -P $(pwd)/Data_Streaming/setup/ https://dlcdn.apache.org/kafka/3.4.1/kafka_2.13-3.4.1.tgz &&
+wget -P $(pwd)/Data_Processing/setup/ https://dlcdn.apache.org/spark/spark-3.4.0/spark-3.4.0-bin-hadoop3.tgz &&
+docker network create --subnet=10.0.100.1/24 tap_project_2 &&
+docker volume create datastorage 
